d47cc717771581916f5708b0385eacbbd8e12d0c
alvaro-j <86482525+alvaro-j@users.noreply.github.com>
2021-06-25 11:54:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var divtempo = document.querySelector('div#tempo')
+divtempo.addEventListener('onload', load())
+
+function load() {
+    var hor = String(new Date().getHours())
+    var min = String(new Date().getMinutes())
+    var sec = String(new Date().getSeconds())
+    var tempo = `${hor} : ${min} : ${sec}`
+    divtempo.innerHTML = tempo
+    setTimeout(load, 1000)
+    if (hor <= 12 && hor >= 5) {
+        document.body.style.backgroundImage = "url('manhã.png')"; 
+    } else if (hor < 18 && hor > 12) {
+        document.body.style.backgroundImage = "url('tarde.jpg')";
+    } else {
+        document.body.style.backgroundImage = "url('noite.png')";
+    }
+}
+
+load()
